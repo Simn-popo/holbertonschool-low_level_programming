f9d0935758f43a090c2f32822fd2ed4386b6ab82
Simn-popo <paulin.simon@live.fr>
2025-02-17 09:18:09 +0100

    5-prinf.c

---
+++ hello_world/5-prinf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * return: Always 0 (Succes)
+ */
+
+int main () {
+printf ("Programming is like building a multilingual puzzle);
+
+return o;
+}
