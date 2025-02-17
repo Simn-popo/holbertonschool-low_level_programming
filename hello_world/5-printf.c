17de57e71f24bfd1090a4d1176cca46b120ba11f
Simn-popo <paulin.simon@live.fr>
2025-02-17 10:10:51 +0100

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 /**
- * main -entry point
- * return: Always 0 (Succes)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-	Printf("with proper grammar, but the outcome is a piece of art,/n");
-      return 0;
-}      
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return 0;
+}
