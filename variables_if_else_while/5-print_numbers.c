6a4e7743132a04fab0cf2d516787e49912e3f317
Simn-popo <paulin.simon@live.fr>
2025-02-18 19:32:50 +0100

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print le 1 le 2 ... le 9
+ *
+ * Return: (0) comme dab
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
