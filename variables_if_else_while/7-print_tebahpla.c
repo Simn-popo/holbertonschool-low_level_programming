efaeabbcd23daa51cfb15be16eabf8218cd62387
Simn-popo <paulin.simon@live.fr>
2025-02-19 10:05:32 +0100

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in reverse
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n >= 123; --n)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+		return (0);
+}
+
