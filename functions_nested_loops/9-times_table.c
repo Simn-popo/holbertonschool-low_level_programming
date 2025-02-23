c0c07bacb5906f13b7cb0785a8aa838e02f03fb3
Simn-popo <paulin.simon@live.fr>
2025-02-23 08:35:03 +0100

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - print times table 0 to 9
+ *
+ * Return: 0
+ */
+
+void times_tables(void)
+{
+	int n, m, o;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			o = n * m;
+			if (o < 10)
+			{
+				if (m > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(o + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(o / 10 + '0');
+				_putchar(o % 10 + '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
