12595c54ff921268c6cdc905f1d9dc2a43953d73
Simn-popo <paulin.simon@live.fr>
2025-02-23 09:30:40 +0100

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,11 +10,13 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
-		for ( ; n < 98; n++)
-			printf("%d, ", n);
-	else
-		for ( ; n > 98; n--)
-			printf("%d, ", n);
-	printf("%d/n", n);
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n < 98)
+			n++;
+		else
+			n--;
+	}
+	printf("98\n");
 }
