57423e40f31e22771a7b4ed9a01952bcc4a32b7b
Simn-popo <paulin.simon@live.fr>
2025-02-27 14:06:13 +0100

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print the string
+ * @str: text
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; n[str] != '\0'; n++)
+		_putchar(n[str]);
+	{
+		_putchar('\n');
+	}
+}
