9b790d9d0ff64da674d0378b93c1236c206b9fa4
Simn-popo <paulin.simon@live.fr>
2025-03-01 12:20:43 +0100

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,3 +8,30 @@
  */
 
 int _atoi(char *s)
+{
+	int n, neg;
+
+	neg = 0;
+	n = 0;
+
+	while (*s < '0' || *s > '9')
+	{
+		if (*s == '-')
+			neg++;
+		if (*s == '\n')
+			return (0);
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		if (n < 0)
+			n *= 10;
+		n -= (int)(*s - '0');
+		s++;
+	}
+
+	if (neg % 2 == 0)
+		n *= -1;
+	return (n);
+}
