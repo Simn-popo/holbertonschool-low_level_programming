88e4099e0bc3edce31a9cc9498d15d56ac224c35
Simn-popo <paulin.simon@live.fr>
2025-03-01 18:27:01 +0100

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+    int i = 0, j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	for (j = 0; src[j] != '\0'; j++)
-		dest[i + j] = src[j];
-	dest[i + j] = src[j];
-	return (dest);
+    while (dest[i] != '\0')
+        i++;
+
+    while (src[j] != '\0') {
+        dest[i + j] = src[j];
+        j++;
+    }
+
+    dest[i + j] = '\0';
+
+    return dest;
 }
