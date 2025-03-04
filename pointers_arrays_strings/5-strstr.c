273995bc2f52c39b7088c65889a5eda78d6d6622
Simn-popo <paulin.simon@live.fr>
2025-03-04 15:05:39 +0100

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/*
+ * _strstr - locates substring
+ * @haystack: the string source
+ * @needle: string to search
+ * Return: to haystack or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *b = haystack;
+	char *k = needle;
+	{
+		while (*b && *k && (*b == *k))
+	{
+		b++;
+		k++;
+	}
+	if (!*k)
+		return (b);
+	b++;
+	}
+	return (NULL);
+}
