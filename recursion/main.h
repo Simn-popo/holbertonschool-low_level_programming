f392e01ac2bdbd2c9741e9ee404b368f51f36224
Simn-popo <paulin.simon@live.fr>
2025-03-05 15:10:58 +0100

    main.h

--- recursion/main.h
+++ recursion/main.h
@@ -1,6 +1,12 @@
-#ifdef main_h
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef main_h
 #define main_h
+
 int _putchar(char c);
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
