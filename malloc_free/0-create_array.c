805cb400ab46084d1bdf4536a6c2738b73e106b2
Simn-popo <paulin.simon@live.fr>
2025-03-10 11:51:31 +0100

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - print array
+ * @size: is a taille
+ * @c: is a char
+ * Return: a array or NULL if 0
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+	unsigned int a;
+
+	char *tab = (char *)malloc(size * sizeof(char));
+
+	if (tab == 0)
+
+	{
+		return ('\0');
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		tab[a] = c;
+	}
+
+	return (tab);
+}
