882dc8f9c8036b12f281a02ad7e94b7301336ecd
Simn-popo <paulin.simon@live.fr>
2025-03-14 11:36:49 +0100

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * init_dog - set doggy 
+ * @d: pointeur de la structure
+ * @name: poppy 
+ * @age: 3.5
+ * @owner: snoop boby dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
