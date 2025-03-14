93d39920e937e5412a7efa1c8f0a4e6d8f41d614
Simn-popo <paulin.simon@live.fr>
2025-03-14 10:55:44 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - set the structure
+ * @name: name dog
+ * @age: ..
+ * @owner: +-*
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+} my_dog;
+
+#endif
