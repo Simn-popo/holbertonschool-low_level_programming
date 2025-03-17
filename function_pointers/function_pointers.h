9cc757c05e86be1e6f7140948eb674258c2389e7
Simn-popo <paulin.simon@live.fr>
2025-03-17 11:18:33 +0100

    function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,4 @@
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+#ifndef salut_h
+#define salut_h
+print_name(char *name, void (*f)(char *));
+#endif
