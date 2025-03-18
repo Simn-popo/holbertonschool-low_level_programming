e000db7dcf46854fa4376fa90f9ee13163200f10
Simn-popo <paulin.simon@live.fr>
2025-03-18 11:53:37 +0100

    3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+/**
+ * op_add - addition
+ * @a: value 1
+ * @b: value 2
+ * Return: addition
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - soustraction
+ * @a: value 1
+ * @b: value 2
+ * Return: soustraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: value 1
+ * @b: value 2
+ * Return: multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: value 1
+ * @b: value 2
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: value 1
+ * @b: value 2
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
