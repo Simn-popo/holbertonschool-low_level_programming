1f519798f3fe6c7d3913e0d7447f4f8537f8959a
Simn-popo <paulin.simon@live.fr>
2025-03-18 12:05:26 +0100

    3-get_op_functions.c

---
+++ function_pointers/3-get_op_functions.c
@@ -0,0 +1,25 @@
+#include "cal.h"
+#include <stdio.h>
+
+/**
+ * get_op_func - selc the good function for calculate
+ * @s: operateur en arg
+ * Return: pointer to a function qui corespond au paramètre des operateurs
+ */
+
+int (*get_op_func(char *s))(int, int)
+const op_t ops[] = {
+{"+", op_add}, {"-", op_sub}, {"*", op_mul},
+{"/", op_div}, {"%", op_mod}, {NULL, NULL}
+};
+
+while (ops[i].op != NULL)
+{
+	if (strcmp(ops[i].op, s) == 0)
+	{
+		return (ops[i].f);
+	}
+	i++;
+}
+
+return (NULL);
