1a1f3c89910d126152ec9537ede31f6835600c8a
Simn-popo <paulin.simon@live.fr>
2025-03-21 11:05:38 +0100

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,5 +6,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void aff_c(va_list aff_list);
+void aff_i(va_list aff_list);
+void aff_f(va_list aff_list);
+void aff_s(va_list aff_list);
 
 #endif
