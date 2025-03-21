74a7c97523a6326a77f1d0880082a5d579222e0b
Simn-popo <paulin.simon@live.fr>
2025-03-21 10:17:42 +0100

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,29 +3,33 @@
 #include <stdio.h>
 
 /**
- * print_numbers - print the numbers
+ * print_strings - print the strings
  * @separator: the string to be print between num
  * @n: number of integers passed in func
  */
 
- void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i
-	int total
-	va_list args;
+	unsigned int i;
+	char *result;
+	va_list aff;
 
-	va_start(args, n);
+	va_start(aff, n);
 
 	for (i = 0; i < n; i++)
 	{
-		total = va_arg(args, char*);
-		if (total ==NULL)
-		total = "(nil)";
-		printf("%s", total);
-
-		if (separator && i < n -1)
-		printf("%s", separator);
+		result = va_arg(aff, char*);
+		if (result == NULL)
+		{
+			printf("(nil)");
+			continue;
+		}
+		printf("%s", result);
+		if (separator != NULL && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
 	}
 	printf("\n");
-	va_end(args);
+	va_end(aff);
 }
