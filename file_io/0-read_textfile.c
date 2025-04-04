352ed9714a13a810733641b47b9ddd46a040ce21
Simn-popo <paulin.simon@live.fr>
2025-04-04 12:10:44 +0200

    fix buffer and betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,25 +13,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *data;
+	char *buffer;
 
-	while(!filename)
-	return (0);
+	if (filename == NULL)
+		return (0);
 
-	data = malloc(sizeof(*data) * letters);
-	if (!data)
-	return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(o, data, letters);
-	w = write(STDOUT_FILENO, data , r);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (0 == -1 || r == -1 || w == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(data);
+		free(buffer);
 		return (0);
 	}
-	free(data);
-	close(0);
+
+	free(buffer);
+	close(o);
+
 	return (w);
 }
